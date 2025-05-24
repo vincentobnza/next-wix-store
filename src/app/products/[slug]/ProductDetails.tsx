fbebaa208f29264bbc6c1a5c4ba2ee92ef129ac7
Vincent Obenza <vincentobenza2003@gmail.com>
2025-05-24 18:55:05 +0800

    Added lens on  Product Image

--- src/app/products/[slug]/ProductDetails.tsx
+++ src/app/products/[slug]/ProductDetails.tsx
@@ -5,12 +5,14 @@ import ProductOptions from "./ProductOptions";
 import { useState } from "react";
 import { checkInStock, findVariant } from "@/lib/utils";
 import ProductPrice from "./ProductPrice";
+import { Lens } from "@/components/ui/lens";
 
 type ProductDetailsProps = {
   product: products.Product;
 };
 
 export default function ProductDetails({ product }: ProductDetailsProps) {
+  const [hovering, setHovering] = useState(false);
   // const [quantity, setQuantity] = useState(1);
   const [selectedOptions, setSelectedOptions] = useState<
     Record<string, string>
@@ -33,14 +35,16 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
 
   return (
     <div className="flex flex-col gap-10 md:flex-row lg:gap-20">
-      <div className="basis-2/5">
-        <WixImage
-          mediaIdentifier={product.media?.mainMedia?.image?.url}
-          alt={product.media?.mainMedia?.image?.altText}
-          width={1000}
-          height={1000}
-          className="sticky top-0"
-        />
+      <div className="basis-2/5 relative">
+        <Lens hovering={hovering} setHovering={setHovering}>
+          <WixImage
+            mediaIdentifier={product.media?.mainMedia?.image?.url}
+            alt={product.media?.mainMedia?.image?.altText}
+            width={1000}
+            height={1000}
+            className="sticky top-0"
+          />
+        </Lens>
       </div>
 
       <div className="basis-3/5 space-y-5">
